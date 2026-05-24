6ce5faf13de867e19d57b8c9362c11fb2f24c67c
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-05-24 09:35:11 -0700

    Tests
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/test2.sh
+++ tests/test2.sh
@@ -8,4 +8,9 @@ cat /tmp/mnt/x509/0/credential-bundle.pem > /tmp/credential-bundle.pem
 [[ $(openssl x509 -noout -in /tmp/credential-bundle.pem | openssl md5) == $(openssl pkey -noout -in /tmp/credential-bundle.pem | openssl md5) ]]
 cat /tmp/mnt/x509/1/credential-bundle.pem > /tmp/credential-bundle.pem
 [[ $(openssl x509 -noout -in /tmp/credential-bundle.pem | openssl md5) == $(openssl pkey -noout -in /tmp/credential-bundle.pem | openssl md5) ]]
-#FIXME check svids
+
+
+main=$(grep -l "main" /tmp/mnt/x509/*/*hint | xargs -I {} dirname {}/credential-bundle.pem)
+other=$(grep -l "other" /tmp/mnt/x509/*/*hint | xargs -I {} dirname {}/credential-bundle.pem)
+openssl x509 -in "$main" -noout -text | grep URI:spiffe://example.org/test2/main
+openssl x509 -in "$other" -noout -text | grep URI:spiffe://example.org/test2/other
