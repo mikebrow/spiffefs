761c07f0b61a86d6045d7023f62deacdfa686efe
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-05-24 09:30:20 -0700

    Tests
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/test2.sh
+++ tests/test2.sh
@@ -5,7 +5,7 @@ ls -l /tmp/mnt/x509/1
 diff -u <(echo main; echo other) <(sort -u /tmp/mnt/x509/*/hint)
 
 cat /tmp/mnt/x509/0/credential-bundle.pem > /tmp/credential-bundle.pem
-[[ $(openssl x509 -noout -modulus -in /tmp/credential-bundle.pem | openssl md5) == $(openssl pkey -noout -modulus -in /tmp/credential-bundle.pem | openssl md5) ]]
+[[ $(openssl x509 -noout -in /tmp/credential-bundle.pem | openssl md5) == $(openssl pkey -noout -in /tmp/credential-bundle.pem | openssl md5) ]]
 cat /tmp/mnt/x509/1/credential-bundle.pem > /tmp/credential-bundle.pem
-[[ $(openssl x509 -noout -modulus -in /tmp/credential-bundle.pem | openssl md5) == $(openssl pkey -noout -modulus -in /tmp/credential-bundle.pem | openssl md5) ]]
+[[ $(openssl x509 -noout -in /tmp/credential-bundle.pem | openssl md5) == $(openssl pkey -noout -in /tmp/credential-bundle.pem | openssl md5) ]]
 #FIXME check svids
