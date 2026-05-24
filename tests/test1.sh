4b84de796510d7aecd41691008277bb7fb4ed084
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-05-24 08:32:30 -0700

    Tests
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/test1.sh
+++ tests/test1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
 ls -l /tmp/mnt/x509/0
-openssl x509 -in /tmp/mnt/x509/0/credential-bundle.pem -noout -text
-openssl x509 -in /tmp/mnt/x509/0/example.org.spiffe-trust-bundle.pem -noout -text
+openssl x509 -in /tmp/mnt/x509/0/credential-bundle.pem -noout -text | grep URI:spiffe://example.org/test1
+openssl x509 -in /tmp/mnt/x509/0/example.org.spiffe-trust-bundle.pem -noout -text | grep URI:spiffe://example.org
