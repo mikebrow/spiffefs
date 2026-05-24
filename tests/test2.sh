8ac9d1c992a28e105fbeffc6caccd862977340e0
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-05-24 09:39:41 -0700

    Tests
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/test2.sh
+++ tests/test2.sh
@@ -10,7 +10,8 @@ cat /tmp/mnt/x509/1/credential-bundle.pem > /tmp/credential-bundle.pem
 [[ $(openssl x509 -noout -in /tmp/credential-bundle.pem | openssl md5) == $(openssl pkey -noout -in /tmp/credential-bundle.pem | openssl md5) ]]
 
 
-main=$(grep -l "main" /tmp/mnt/x509/*/*hint | xargs -I {} dirname {}/credential-bundle.pem)
+main=$(dirname "$(grep -l "main" /tmp/mnt/x509/*/*hint 2>/dev/null | head -n 1)")
 other=$(grep -l "other" /tmp/mnt/x509/*/*hint | xargs -I {} dirname {}/credential-bundle.pem)
-openssl x509 -in "$main" -noout -text | grep URI:spiffe://example.org/test2/main
-openssl x509 -in "$other" -noout -text | grep URI:spiffe://example.org/test2/other
+other=$(dirname "$(grep -l "other" /tmp/mnt/x509/*/*hint 2>/dev/null | head -n 1)")
+openssl x509 -in "$main/credential-bundle.pem" -noout -text | grep URI:spiffe://example.org/test2/main
+openssl x509 -in "$other/credential-bundle.pem" -noout -text | grep URI:spiffe://example.org/test2/other
