925f4968e4ce9053f4bf4104ee613730f01854ba
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-05-24 09:22:22 -0700

    Tests
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/test2.sh
+++ tests/test2.sh
@@ -4,8 +4,8 @@ ls -l /tmp/mnt/x509/0
 ls -l /tmp/mnt/x509/1
 diff -u <(echo main; echo other) <(sort -u /tmp/mnt/x509/*/hint)
 
-cp /tmp/mnt/x509/0/credential-bundle.pem /tmp
+cat /tmp/mnt/x509/0/credential-bundle.pem > /tmp/credential-bundle.pem
 [[ $(openssl x509 -noout -modulus -in /tmp/credential-bundle.pem | openssl md5) == $(openssl rsa -noout -modulus -in credential-bundle.pem | openssl md5) ]]
-cp /tmp/mnt/x509/1/credential-bundle.pem /tmp
+cat /tmp/mnt/x509/1/credential-bundle.pem > /tmp/credential-bundle.pem
 [[ $(openssl x509 -noout -modulus -in /tmp/credential-bundle.pem | openssl md5) == $(openssl rsa -noout -modulus -in credential-bundle.pem | openssl md5) ]]
 #FIXME check svids
